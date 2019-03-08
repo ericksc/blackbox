37d63b14686c262677f7b224e84894c38a436aef
Erick Salas-Chaverri <ericksc@gmail.com>
2019-03-08 14:49:38 -0600

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='blackbox',
+    version='1.0',
+    packages=['blackbox'],
+    url='',
+    license='',
+    author='ecsa',
+    author_email='',
+    description=''
+)
